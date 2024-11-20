287326c1c12303c87be2b70d9d415fdc6cc98c14
sree1223 <sreeharsha120203@gmail.com>
2024-11-20 17:14:52 +0530

    Harsha update

--- GKB-Customization/tomain.py
+++ GKB-Customization/tomain.py
@@ -2,11 +2,11 @@ import os
 
 cmds = '''cd..
 git add .
-git commit -m "Your commit message"
+git commit -m "Harsha update"
 git checkout main
-git merge --no-ff v11
+git merge --no-ff v12
 git push origin main
-git checkout v11'''
+git checkout v12'''
 for i in cmds.splitlines():
   os.system(i)
 
